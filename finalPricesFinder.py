22050a459b3fdc2846620dc6fe66b2b972488b98
LAPTOP-KC2QNK2P\joris <j.f.vlaar@student.utwente.nl>
2021-10-14 16:29:49 +0200

    American put

--- finalPricesFinder.py
+++ finalPricesFinder.py
@@ -9,3 +9,16 @@ def find_final_price(price, u, d, periods):
         iterPrice = (iterPrice / u) * d
         finalPrices.append(iterPrice)
     return finalPrices
+
+
+def find_all_prices(price, u, d, periods):
+    prices = [price]
+    stepPrice = price
+    for i in range(periods):
+        stepPrice = stepPrice * u
+        prices.append(stepPrice)
+        iterPrice = stepPrice
+        for j in range(i + 1):
+            iterPrice = (iterPrice / u) * d
+            prices.append(iterPrice)
+    return prices
