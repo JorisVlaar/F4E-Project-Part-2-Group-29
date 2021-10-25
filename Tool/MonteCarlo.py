8a4d5837ae06967307f1a72c2c7010c2303ffd9d
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-10-25 12:32:48 +0200

    eu-put/call monte carlo, put/call looback (fixed strike)

--- Tool/MonteCarlo.py
+++ Tool/MonteCarlo.py
@@ -23,15 +23,48 @@ dt = 30/365          # timeperiod
 N = 60000            # number of simulations
 n =  3               # number of steps
 TTM = 90/365         # time to maturity
-
+OptionType = "Lookback"      
+CallPut = "Call"
 
 paths = simulation_path(P0, r, v, dt, N)
 
+if OptionType == "EU":
+    if CallPut == "Call":
+           payoffs = np.maximum(paths[-1]-K, 0)
+    elif CallPut == "Put":
+           payoffs = np.maximum(K-paths[-1], 0)
+elif OptionType == "US":
+    if CallPut == "Call":
+       None     
+    elif CallPut == "Put":
+       None      
+elif OptionType == "Asian":
+    if CallPut == "Call":
+       None    
+    elif CallPut == "Put":
+       None      
+elif OptionType == "Bermudan":
+    if CallPut == "Call":
+       None
+    elif CallPut == "Put":
+       None
+elif OptionType == "Chooser":
+       None
+elif OptionType == "LookBack":
+    if CallPut == "Call":
+       max_=np.max(paths,axis=0)
+       payoffs = np.maximum(max_-K, 0)
+    elif CallPut == "Put":
+       min_=np.min(paths,axis=0)
+       payoffs = np.maximum(K-min_, 0)
+elif OptionType == "Barrier":
+    pass
 
 
-payoffs = np.maximum(paths[-1]-K, 0)
 option_price = np.mean(payoffs)*np.exp(-r*TTM) # discounting back to present value
 print(option_price)
+
+
 
 
 
