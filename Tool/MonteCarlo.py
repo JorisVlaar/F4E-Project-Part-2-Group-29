a7bbc79fcf111614ea237f7c04e3f0b1642b7a08
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-10-25 12:47:57 +0200

    other asian implemenation

--- Tool/MonteCarlo.py
+++ Tool/MonteCarlo.py
@@ -1,17 +1,12 @@
 import numpy as np
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def simulation_path(P0, r, v, dt, N):
     Pt = np.log(P0) + np.cumsum(((r - ((v**2)/2))*dt +
-                                                     v*np.sqrt(dt) * np.random.normal(size=(n, N))), axis=0)
+                                 v*np.sqrt(dt) * np.random.normal(size=(n, N))), axis=0)
     Pt = np.round_(Pt, decimals=2)
-    return np.exp(Pt) 
-
-
-
-
-
+    return np.exp(Pt)
 
 
 # inputs
@@ -21,52 +16,55 @@ r = 0.05             # annual interest rate
 v = 0.25             # volatility
 dt = 30/365          # timeperiod
 N = 60000            # number of simulations
-n =  3               # number of steps
+n = 3               # number of steps
 TTM = 90/365         # time to maturity
-OptionType = "Lookback"      
+OptionType = "Lookback"
 CallPut = "Call"
 
 paths = simulation_path(P0, r, v, dt, N)
 
 if OptionType == "EU":
     if CallPut == "Call":
-           payoffs = np.maximum(paths[-1]-K, 0)
+        payoffs = np.maximum(paths[-1]-K, 0)
     elif CallPut == "Put":
-           payoffs = np.maximum(K-paths[-1], 0)
+        payoffs = np.maximum(K-paths[-1], 0)
 elif OptionType == "US":
     if CallPut == "Call":
-       None     
+        None
     elif CallPut == "Put":
-       None      
+        None
 elif OptionType == "Asian":
+    avg_ = np.average(paths, axis=0)
+#     if CallPut == "Call":
+#         payoffs = np.max(avg_-K, 0)
+#     elif CallPut == "Put":
+#         payoffs = np.max(K-avg_, 0)
     if CallPut == "Call":
-       None    
+        payoffs = np.maximum(paths[-1]-avg_, 0)
     elif CallPut == "Put":
-       None      
+        payoffs = np.maximum(avg_-paths[-1], 0)
+
 elif OptionType == "Bermudan":
     if CallPut == "Call":
-       None
+        None
     elif CallPut == "Put":
-       None
+        None
 elif OptionType == "Chooser":
-       None
+    None
 elif OptionType == "LookBack":
     if CallPut == "Call":
-       max_=np.max(paths,axis=0)
-       payoffs = np.maximum(max_-K, 0)
+        max_ = np.max(paths, axis=0)
+        payoffs = np.maximum(max_-K, 0)
     elif CallPut == "Put":
-       min_=np.min(paths,axis=0)
-       payoffs = np.maximum(K-min_, 0)
+        min_ = np.min(paths, axis=0)
+        payoffs = np.maximum(K-min_, 0)
 elif OptionType == "Barrier":
     pass
 
 
-option_price = np.mean(payoffs)*np.exp(-r*TTM) # discounting back to present value
+# discounting back to present value
+option_price = np.mean(payoffs)*np.exp(-r*TTM)
 print(option_price)
-
-
-
-
 
 
 # plt.plot(paths)
