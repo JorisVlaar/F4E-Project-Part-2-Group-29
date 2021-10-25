af367db26805ad309f9c528f0f1750e3a805528d
MvE <70101583+CRYSTALCRYSTALC@users.noreply.github.com>
2021-10-25 11:17:29 +0200

    Create MonteCarlo.py

---
+++ Tool/MonteCarlo.py
@@ -0,0 +1,20 @@
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+def simulation_path(PStock, interest, volatilty, periodLength, N):
+       PstockFuture = numpy.log(PStock) + numpy.cumsum(((r - sigma**2/2)*periodLength) + volatilty*numpy.sqrt(periodLength) * numpy.random.normal(size=(steps,N))),axis=0)
+       return PstockFuture
+
+#inputs
+PStock = 50
+interest - 0.1
+volatilty = 0.4
+periodLength = 30/365
+N = 100
+
+paths = simulation_path(PStock, interest, volatilty, periodLength, N)
+
+plt.plot(paths);
+plt.xlabel("Time Increments")
+plt.ylabel("Stock Price")
+plt.title("Geometric Brownian Motion")
