7be3e0d14a9bc12fd821ba7b9a23ec5720c1c661
Pablito-Escobar-dev <daniel.lizarazo.fuentes@antipy.com>
2021-10-25 12:14:48 +0200

    monte carlo working EU call

--- Tool/MonteCarlo.py
+++ Tool/MonteCarlo.py
@@ -1,24 +1,42 @@
-import numpy as numpy
-import matplotlib.pyplot as plt
+import numpy as np
+#import matplotlib.pyplot as plt
 
-def simulation_path(PStock, interest, volatilty, periodLength, N):
-       PstockFuture = numpy.log(PStock) + numpy.cumsum(((r - sigma**2/2)*periodLength) + volatilty*numpy.sqrt(periodLength) * numpy.random.normal(size=(steps,N))),axis=0)
-       PstockFuture = round(PstockFuture, decimals = 2)
-       return PstockFuture
 
-#inputs
-PStock = 50
-interest - 0.1
-volatilty = 0.4
-periodLength = 30/365
-N = 100
-maturity = 90/365
+def simulation_path(P0, r, v, dt, N):
+    Pt = np.log(P0) + np.cumsum(((r - ((v**2)/2))*dt +
+                                                     v*np.sqrt(dt) * np.random.normal(size=(n, N))), axis=0)
+    Pt = np.round_(Pt, decimals=2)
+    return np.exp(Pt) 
 
-steps = maturity/periodLength
 
-paths = simulation_path(PStock, interest, volatilty, periodLength, N)
 
-plt.plot(paths);
-plt.xlabel("Time Increments")
-plt.ylabel("Stock Price")
-plt.title("Geometric Brownian Motion")
+
+
+
+
+# inputs
+P0 = 100             # initial stock price
+K = 110              # strike
+r = 0.05             # annual interest rate
+v = 0.25             # volatility
+dt = 30/365          # timeperiod
+N = 60000            # number of simulations
+n =  3               # number of steps
+TTM = 90/365         # time to maturity
+
+
+paths = simulation_path(P0, r, v, dt, N)
+
+
+
+payoffs = np.maximum(paths[-1]-K, 0)
+option_price = np.mean(payoffs)*np.exp(-r*TTM) # discounting back to present value
+print(option_price)
+
+
+
+
+# plt.plot(paths)
+# plt.xlabel("Time Increments")
+# plt.ylabel("Stock Price")
+# plt.title("Geometric Brownian Motion")
